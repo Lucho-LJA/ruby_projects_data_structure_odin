5958b5c7190fd6a814e3ce737ae2ba5d311e7ef6
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-22 17:51:14 -0500

    Add test file to knights_travails

---
+++ tests/test_knights_travails.rb
@@ -0,0 +1,6 @@
+require './lib/knights_travails'
+
+Board.new.knight_moves([3,3],[4,3])
+Board.new.knight_moves([0,0],[7,7])
+Board.new.knight_moves([1,0],[3,7])
+Board.new.knight_moves([0,0],[7,0])
